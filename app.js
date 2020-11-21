91fde7444494b37070d590360494f8272ddfb49f
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:49:36 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('Datos Personales')
+
+alert('Creación de usuario')
+
+const nombre = prompt('Ingresa tu nombre');
+const apellido = prompt('Ingresa tu apellido');
+const edad = prompt('Ingresa tu edad');
+const nacionalidad = prompt('Ingresa tu nacionalidad');
+const documento = prompt('Ingresa tu documento');
+
+alert(`Nuevo usuario agregado al sistema: 
+    Nombre: ${nombre}, 
+    Apellido: ${apellido},
+    Edad: ${edad}, 
+    Nacionalidad: ${nacionalidad}, 
+    Documento: ${documento}.
+    `)
